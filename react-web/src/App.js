9ab4becab593907b08c5a006e13fa7e3c7e9d7e6
Michael Bamford <michael@michaelbamford.com>
2017-12-10 18:54:36 +1100

    Add MovieForm

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import MoviesList from './components/MovieList'
-
+import MovieForm from './components/MovieForm'
+import * as moviesAPI from './api/movies'
 class App extends Component {
   state = { movies: null }
 
   componentDidMount() {
-    fetch('/movies')
-      .then(res => res.json())
+    moviesAPI.all()
       .then(movies => {
         this.setState({ movies })
       })
-      .catch(error => { console.log(error) })
+  }
+
+  handleMovieSubmission = (movie) => {
+    this.setState(({ movies }) => (
+      { movies: [ movie ].concat(movies) }
+    ));
+
+    moviesAPI.save(movie);
   }
 
   render() {
     const { movies } = this.state;
     return (
       <div className="App">
+        <MovieForm onSubmit={this.handleMovieSubmission} />
+        <hr/>
         {
           movies ? (
             <MoviesList movies={ movies } />
