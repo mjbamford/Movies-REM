fb398614f8befd005e6450510971af9fa1fbb45e
Luke Schoen <ltfschoen@gmail.com>
2017-12-14 07:05:31 +1100

    tests: Attempt to implement Jest and Enzyme using Mock Function API

--- react-web/src/App.test.js
+++ react-web/src/App.test.js
@@ -1,8 +1,34 @@
+// Jest
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('calls the mock moviesAPI.all() function', () => {
+  jest.mock('./api/movies');
+  const allMockFunction = jest.fn().mockName('allMockFunction')
+  App.mockImplementation(() => {
+    return {
+      all: allMockFunction
+    }
+  })
+  
+  const app = new App();
+  app.all();
+  expect(allMockFunction).toHaveBeenCalled();
 });
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
+
+// Enzyme
+
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import App from './App';
+
+// it('renders without crashing', () => {
+//   shallow(<App />);
+// });
